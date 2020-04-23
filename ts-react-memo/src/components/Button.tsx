7149803257cb7737f7d826a97b48211d9b9c2ab8
choiwono <treasure_b@naver.com>
2020-04-23 23:54:05 +0900

    :sparkles: typescript 연습

--- ts-react-memo/src/components/Button.tsx
+++ ts-react-memo/src/components/Button.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-
+import styled from 'styled-components';
 
 interface ButtonProps {
     to ? : string;
@@ -8,16 +8,22 @@ interface ButtonProps {
     onClick ? () : void;
 }
 
-const Button : React.FC<ButtonProps> = (props) => {
+const ButtonStyle = styled.button<ButtonProps>`
+  display: inline-block;
+  border: solid 1px #ccc;
+  border-color: ${props => props.primary ? "#2e6da4" : "#ccc"};
+`;
+
+const Button : React.FC<ButtonProps> = props => {
     const { to, children, primary, onClick } = props;
     const isLink = !!to;
 
     const renderButton = () => {
-        return (
-            <button
-                style={}
-            >
-            </button>
+        return ( 
+            <ButtonStyle primary={true}
+                onClick={onClick}
+            >{children}
+            </ButtonStyle>
         )
     }
 
@@ -25,19 +31,6 @@ const Button : React.FC<ButtonProps> = (props) => {
         if (!to) {
             return <div></div>
         }
-
-        return (
-            <Link
-            >
-
-            </Link>
-        )
-    }
-}
-
-const buttonStyle = (primary: boolean) => {
-    return {
-
     }
 }
 
